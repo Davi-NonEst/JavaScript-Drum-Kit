c42067e19aed5c5a7bd9939042b216624731ea62
Davi Alves <davialvesvilar2010@gmail.com>
2026-01-03 19:12:38 -0300

    :recycle: simplified key/audio selector

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+function getKeyElement(code){
+    const key = document.querySelector(`.key[data-key="${code}"]`)
+    return key
+}
+
 window.addEventListener('keydown', (event) => {
-    const key = document.querySelector(`.key[data-key="${event.code}"]`) //Seleciona a .key que possui o atributo data-key igual ao event.code, que é a tecla clicada 
-    const audio = document.querySelector(`.key[data-key="${event.code}"] audio`)
+    const key = getKeyElement(event.code)  //Seleciona a .key que possui o atributo data-key igual ao event.code, que é a tecla clicada 
+
+    if (!key) return //Se key for null ou algo do tipo, return
+
+    const audio = key.querySelector(`audio`) //A busca começa em key
 
     if (!audio) return //Se não conseguir achar o audio, return
 
@@ -10,6 +18,9 @@ window.addEventListener('keydown', (event) => {
 })
 
 window.addEventListener('keyup', (event) => {
-    const key = document.querySelector(`.key[data-key="${event.code}"]`)
+    const key = getKeyElement(event.code)
+
+    if(!key) return
+
     key.classList.remove('keydown')
 })
